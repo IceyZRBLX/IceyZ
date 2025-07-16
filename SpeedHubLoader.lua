7f4961e2359fcf7a29c33f70f842bb016e80cddd
IceyZRBLX <jaydenxd5o4@gmail.com>
2025-07-16 22:36:33 +0700

    Update SpeedHubLoader.lua

--- SpeedHubLoader.lua
+++ SpeedHubLoader.lua
@@ -1,1 +1,8 @@
 
+--[[
+🚀 Speed Hub Loader
+📦 Loader resmi dari IceyZRBLX
+🌐 Mengambil script utama dari AhmadV99
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
